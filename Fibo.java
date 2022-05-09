933fc340f04b29cea79941b06e7d2cf75808b3c5
Santosh-mv <jsanthosh16227@gmail.com>
2022-05-09 22:16:34 +0530

    First step
    First step

---
+++ Fibo.java
@@ -0,0 +1,18 @@
+public class Fibo
+{
+public static void main(String [] args){
+
+    int a= fibo(6);
+    System.out.print(a);
+}
+static int fibo(int n)
+{
+    if(n<2)
+    {
+        return n;
+    }
+    return fibo(n-1)+fibo(n-2);
+
+}
+
+}
